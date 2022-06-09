7267c638e03ea14f17237430b15b3b719b68f16c
Justin James <justinmjames@protonmail.com>
2022-06-08 21:12:43 -0700

    commit tree 1da450d3aa89a69a8e8ddd8174c3d85f5d811469

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+
+function submitData(userName, userEmail){
+    let userObj = {
+        name: userName,
+        email: userEmail,
+    }
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json',
+        },
+        body:JSON.stringify(userObj)
+    })
+        .then(res => res.json())
+        .then(object => {
+           const body = document.querySelector('body');
+           const h4 = document.createElement('h4');
+           h4.textContent = object.id;
+           body.append(h4);
+        })
+        .catch((error) => {
+           let message = `${error} Unauthorized Access`
+           document.querySelector('body').append(message);
+           
+        });
+       
+    }
+
+    
